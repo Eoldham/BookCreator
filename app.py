1e568e6e0b92905baef2fd8a19e4c0c1fd3d7f3c
Eoldham <owlanne@udel.edu>
2019-10-15 22:49:58 -0400

    lots of new features

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask,render_template,redirect, url_for,request
-
-
+import Character_class
+from forms import SignUpForm
+from forms import Character_sheet
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'thecodex'
 
+character_list = []
+#global list that keeps all character dictionaries created
 @app.route('/')
 def home():
     return render_template('home.html')
@@ -23,17 +27,35 @@ def pick_plot():
 
 @app.route('/setting')
 def pick_setting():
+    error = None
+    if request.method == 'POST':
+        name = request.form['name']
+
     return render_template('setting.html')
 
-@app.route('/characters')
+@app.route('/characters', methods = ['GET','POST'])
 def characters():
-    return render_template('characters.html')
+    form = Character_sheet()
+    if form.is_submitted():
+        character = request.form
+        character_list.append(character)
+    return render_template('characters.html', form = form)
+
+@app.route('/user_character')
+def characters():
+    return render_template('user_characters.html', character_list = character_list)
 
 @app.route('/write')
 def writing():
     return render_template('write.html')
 
-
+@app.route('/signup',methods = ['GET', 'POST'])
+def signup():
+    form = SignUpForm()
+    if form.is_submitted():
+        result = request.form
+        return render_template('user.html', result = result)
+    return render_template('signup.html', form = form)
 
 if __name__ == '__main__':
     app.run()
---
+++ forms.py
@@ -0,0 +1,35 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+
+
+class SignUpForm(FlaskForm):
+    username = StringField('Username')
+    password = PasswordField('Password')
+    submit = SubmitField('Sign up')
+
+
+class Character_sheet(FlaskForm):
+    name = StringField('Name')
+    nickname = StringField('Nickname')
+    birthday = StringField('Birthday')
+    birth_place = StringField('Birth Place')
+    height = StringField('Height')
+    weight = StringField('Weight')
+    body = StringField('Body Shape')
+    eye_color = StringField('Eye Color')
+    glasses = StringField('Glasses?')
+    contacts = StringField('Contacts?')
+    skin_tone = StringField('Skin Tone')
+    freckles = StringField('Freckles?')
+    moles = StringField('Moles?')
+    mole_placement = StringField('Mole Placement')
+    scars = StringField('Scars?')
+    scar_placement = StringField('Scar Placement')
+    tattoos = StringField('Tattoos')
+    tattoo_placement = StringField('Tattoo Placement')
+    health = StringField('Health')
+    outfit = StringField('Outfit')
+    hair_color = StringField('Hair Color')
+    hair_texture = StringField('Hair Texture')
+    hair_length = StringField('Hair Length')
+    submit = SubmitField('Create Character')
